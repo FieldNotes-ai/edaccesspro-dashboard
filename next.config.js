53d4a476f8309a2cafdb6db9c8e89280d58eec89
Andrew Lewis <andrewlewis@Andrews-MacBook-Pro.local>
2025-07-06 05:19:56 -0400

    Simplify Next.js config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  trailingSlash: true,
-  output: 'standalone'
+  swcMinify: true
 };
 
 module.exports = nextConfig;
